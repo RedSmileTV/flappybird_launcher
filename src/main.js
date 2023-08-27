66b694d87e02b4ec49591c8ebd6b7a59d8e02560
RedSmile <lelledefeo@gmail.com>
2023-08-27 03:12:10 +0200

    Added version selection and added Authors tab

--- src/main.js
+++
@@ -1,7 +0,0 @@
-const { invoke } = window.__TAURI__;
-
-const playButton = document.getElementById("launch-btn");
-
-playButton.addEventListener("click", async () => {
-  await invoke("launch");
-});
